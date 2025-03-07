bc22c3f0603d2d4bf031c17c3830fdc12c75f851
Muhamad Fudhail <136212616+MuhammadFudhail@users.noreply.github.com>
2025-03-07 09:30:15 +0800

    menambahkan folder gambar

--- dashboard_telkom/src/App.jsx
+++ dashboard_telkom/src/App.jsx
@@ -3,12 +3,14 @@ import Login from './Pages/Login'
 
 
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>BISMILLAH GESS!!</h1>
+    <h1 className='text-red-500'>BISMILLAH GESS!!</h1>
+    <h2 className='text-9xl'>halo</h2>
     <Login />
     </>
   )
